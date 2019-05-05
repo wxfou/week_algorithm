7019a523baf4415d768bb414b58770d69951b47f
wxfou <wxf_ou@163.com>
2019-05-05 22:50:44 +0800

    Update NB_text_class.py

--- NB_text_class.py
+++ NB_text_class.py
@@ -3,7 +3,7 @@ from numpy import *
 import re
 import random
  
-def loadDataSet(): #创建样例数据
+def loadDataSet():
      postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
                    ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
@@ -13,13 +13,13 @@ def loadDataSet(): #创建样例数据
      classVec = [0, 1, 0, 1, 0, 1]  #1代表脏话
      return postingList, classVec
 
-def createVocabList(dataSet):  #创建词库 这里就是直接把所有词去重后，当作词库
+def createVocabList(dataSet): 
      vocabSet = set([])
      for document in dataSet:
          vocabSet = vocabSet | set(document)
      return list(vocabSet)
 
-def setOfWords2Vec(vocabList, inputSet):  #文本词向量。词库中每个词当作一个特征，文本中就该词，该词特征就是1，没有就是0
+def setOfWords2Vec(vocabList, inputSet): 
      returnVec = [0] * len(vocabList)
      for word in inputSet:
          if word in vocabList:
@@ -32,7 +32,7 @@ def trainNB0(trainMatrix, trainCategory):
      numTrainDocs = len(trainMatrix)
      numWords = len(trainMatrix[0])
      pAbusive = sum(trainCategory) / float(numTrainDocs)
-     p0Num = ones(numWords) #防止某个类别计算出的概率为0，导致最后相乘都为0，所以初始词都赋值1，分母赋值为2.
+     p0Num = ones(numWords)
      p1Num = ones(numWords)
      p0Denom = 2
      p1Denom = 2
@@ -43,11 +43,11 @@ def trainNB0(trainMatrix, trainCategory):
          else:
              p0Num += trainMatrix[i]
              p0Denom += sum(trainMatrix[i])
-     p1Vect = log(p1Num / p1Denom)  #这里使用了Log函数，方便计算，因为最后是比较大小，所有对结果没有影响。
+     p1Vect = log(p1Num / p1Denom) 
      p0Vect = log(p0Num / p0Denom)
      return p0Vect, p1Vect, pAbusive
 
-def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1): #比较概率大小进行判断，
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
      p1 = sum(vec2Classify*p1Vec)+log(pClass1)
      p0 = sum(vec2Classify*p0Vec)+log(1-pClass1)
      if p1>p0:
@@ -62,10 +62,10 @@ def testingNB():
      for postinDoc in listOPosts:
          trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
      p0V,p1V,pAb = trainNB0(array(trainMat),array(listClasses))
-     testEntry = ['love', 'my', 'dalmation'] # 测试数据
+     testEntry = ['love', 'my', 'dalmation'] 
      thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
      print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
-     testEntry = ['stupid', 'garbage'] # 测试数据
+     testEntry = ['stupid', 'garbage'] 
      thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
      print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
 
